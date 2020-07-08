06f1592d9b2f9dc588ce9d0a36d6096e74067173
Sel <gmscott27@gmail.com>
2020-07-08 04:20:33 -0400

    Refactoring to push all the initialization into a self contained function.

--- TT_FBX/src/TT_FBX.cpp
+++ TT_FBX/src/TT_FBX.cpp
@@ -14,97 +14,145 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdio.h>
 #include <memory>
 
 const char* initScript = "SQL/CreateDB.SQL";
 const char* dbPath = "result.db";
 
-/**
- * Main function - loads the hard-coded fbx file,
- * and prints its contents in an xml format to stdout.
+
+inline bool file_exists(const std::string& name) {
+
+	std::fstream fs;
+	fs.open(name, std::fstream::in);
+	if (fs.fail()) {
+		return false;
+	}
+	fs.close();
+	return true;
+}
+
+
+/* 
+ * Attempts to initialize the SQLite Database and FBX scene.
+ * Returns 0 on success, non-zero on error.
  */
-int main(int argc, char* argv[]) {
+int Init(const int argc, const char* argv[], sqlite3* db, FbxManager *manager, FbxScene* scene) {
+
 	if (argc < 2) {
 		fprintf(stderr, "No file path supplied.\n");
-		return(1);
+		return(101);
 	}
 	else {
 		fprintf(stdout, "Attempting to process FBX: %s\n", argv[1]);
 	}
 	const char* fbxFilePath = argv[1];
 
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
+	char *zErrMsg = 0;
+	int rc;
 
-	std::unique_ptr<char> v1 = std::make_unique<char>();
+	if (file_exists(dbPath)) {
+		int failure = remove(dbPath);
+		if (failure != 0) {
+			fprintf(stderr, "Unable to remove existing database.\n");
+			return(102);
+		}
+
+	}
+
+	// Create and connect to the database file.
+	rc = sqlite3_open(dbPath, &db);
+	if (rc) {
+		fprintf(stderr, "Failed to create database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return(103);
+	}
 
 
-    rc = sqlite3_open(dbPath, &db);
-    if( rc ){
-      fprintf(stderr, "Failed to create database: %s\n", sqlite3_errmsg(db));
-      sqlite3_close(db);
-      return(13);
-    }
+	// Read the DB Creation file.
+	std::ifstream file(initScript);
+	std::string str;
+	std::string fullSql = "";
+	while (std::getline(file, str))
+	{
+		fullSql += str + "\n";
+	}
+	file.close();
 
-	
-	
-	/*
-    rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-    if( rc!=SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-     sqlite3_free(zErrMsg);
-    }*/
-    sqlite3_close(db);
-    //return 0;
+	// Create the DB Schema.
+	rc = sqlite3_exec(db, fullSql.c_str(), NULL, 0, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Database creation SQL error: ", zErrMsg);
+		sqlite3_free(zErrMsg);
+		sqlite3_close(db);
+		return 104;
+	}
 
 
 
 	// Create the FBX SDK manager
-	FbxManager* lSdkManager = FbxManager::Create();
+	manager = FbxManager::Create();
 
 	// Create an IOSettings object.
-	FbxIOSettings * ios = FbxIOSettings::Create(lSdkManager, IOSROOT);
-	lSdkManager->SetIOSettings(ios);
-
-	// ... Configure the FbxIOSettings object ...
-	/*(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_MATERIAL, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_TEXTURE, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_LINK, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_SHAPE, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_GOBO, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_ANIMATION, false);
-	(*(lSdkManager->GetIOSettings())).SetBoolProp(IMP_FBX_GLOBAL_SETTINGS, true);*/
+	FbxIOSettings * ios = FbxIOSettings::Create(manager, IOSROOT);
+	manager->SetIOSettings(ios);
 
 	// Create an importer.
-	FbxImporter* lImporter = FbxImporter::Create(lSdkManager, "");
+	FbxImporter* importer  = FbxImporter::Create(manager, "");
 
 	// Declare the path and filename of the fidle containing the scene.
 	// In this case, we are assuming the file is in the same directory as the executable.
 
 	// Initialize the importer.
-	bool lImportStatus = lImporter->Initialize(fbxFilePath, -1, lSdkManager->GetIOSettings());
+	bool success = importer->Initialize(fbxFilePath, -1, manager->GetIOSettings());
 
 	// Use the first argument as the filename for the importer.
-	if (!lImportStatus) {
-		printf("Call to FbxImporter::Initialize() failed.\n");
-		printf("Error returned: %s\n\n", lImporter->GetStatus().GetErrorString());
-		exit(-1);
+	if (!success) {
+		fprintf(stderr, "Unable to load FBX file.");
+		sqlite3_close(db);
+		manager->Destroy();
+		return 105;
 	}
 
 	// Create a new scene so that it can be populated by the imported file.
-	FbxScene* lScene = FbxScene::Create(lSdkManager, "myScene");
+	scene = FbxScene::Create(manager, "root");
 
 	// Import the contents of the file into the scene.
-	lImporter->Import(lScene);
+	importer->Import(scene);
 
 	// The file is imported; so get rid of the importer.
-	lImporter->Destroy();
+	importer->Destroy();
 
-	// Destroy the SDK manager and all the other objects it was handling.
-	lSdkManager->Destroy();
+	return 0;
+}
 
-	char z;
-	//std::cin >> z;
+/**
+ * Program entry point, yaaaay.
+ */
+int main(int argc, char* argv[]) {
+
+
+	FbxManager* manager;
+	FbxScene* scene;
+	sqlite3* db;
+
+	// Try to load all the things.
+	int result = Init(argc, argv, db, manager, scene);
+	if (result != 0) {
+		return result;
+	}
+
+	// We're now ready to actually do some work.
+
+
+
+
+	// Destroying the manger destroys the scene with it.
+	manager->Destroy();
+	
+	// Good night DB.
+	sqlite3_close(db);
+
+	// Great Success.
 	return 0;
 }
